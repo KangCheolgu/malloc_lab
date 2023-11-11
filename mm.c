93995222ad55862220432428f9544d6ce0b72a25
KangCheolgu <opopqwqw123@gmail.com>
2023-11-11 12:27:47 +0900

    기본 말록 구현

--- mm.c
+++ mm.c
@@ -24,11 +24,11 @@
  ********************************************************/
 team_t team = {
     /* Team name */
-    "ateam",
+    "5 week 5 team",
     /* First member's full name */
-    "Harry Bovik",
+    "Kang Cheolgu",
     /* First member's email address */
-    "bovik@cs.cmu.edu",
+	"opopqwqw123@gmail.com",
     /* Second member's full name (leave blank if none) */
     "",
     /* Second member's email address (leave blank if none) */
@@ -36,26 +36,155 @@ team_t team = {
 };
 
 /* single word (4) or double word (8) alignment */
+
 #define ALIGNMENT 8
 
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 
-
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+
+#define WSIZE 4										// 워드 , 헤더, 푸터 사이즈 (바이트)
+#define DSIZE 8										// 더블 워드 사이즈 (바이트)
+#define CHUNKSIZE (1<<12)							// 이 양 만큼 힙을 확장해라
+
+#define MAX(x,y) ((x) > (y)? (x) : (y))				// 둘중에 큰거 return 
+#define PACK(size, alloc) ((size) | (alloc))		// PACK 매크로 사용하여 크기와 할당 여부를 하나의 워드로 패킹
+
+/*
+PACK 매크로는 크기(size)와 할당 여부(alloc)를 받아서 이를 하나의 워드로 패킹합니다.
+size는 하위 비트에 위치하며, alloc은 상위 비트에 위치합니다.
+| 비트 OR 연산자를 사용하여 두 값을 결합합니다.
+
+size_t size = 16;   // 예시로 크기를 16으로 설정
+int alloc = 1;      // 예시로 할당 여부를 1로 설정 (할당됨)
+
+// PACK 매크로 사용하여 크기와 할당 여부를 하나의 워드로 패킹
+
+word_t packedValue = PACK(size, alloc);
+
+// 이제 packedValue는 size와 alloc 정보가 패킹된 하나의 워드를 나타냄
+
+*/
+
+// p 의 주소 word를 읽고 씀
+#define GET(p)		(*(unsigned int *) (p))
+#define PUT(p, val)	(*(unsigned int *)(p) = (val))
+
+// 주소 p 로부터 크기를 읽고 필드를 할당
+#define GET_SIZE(p)	(GET(p) & -0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+// 특정 메모리 블록에 대한 포인터 bp가 주어졌을 때, 해당 블록의 헤더와 푸터의 주소를 계산하라는 의미
+#define HDRP(dp)	((char *)(bp) - WSIZE)
+#define FTRP(dp)	((char *)(bp) + GET_SIZE(HDRP(bp)) - WSIZE)
+
+// 특정 메모리 블록에 대한 포인터 bp가 주어졌을 때, 다음 블록의 헤더와 푸터의 주소를 계산하라는 의미
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+
+
 
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+	// 빈 initial heap 을 만든다.	
+	if (( heap_listp = mem_sbrk(4 * WSIZE)) == (void *) - 1) return -1;
+
+	PUT(heap_listp, 0);								// Alignment padding
+	PUT(heap_listp + (1 * WSIZE), PACK(DSIZE,1)); // 프롤로그의 헤더
+	PUT(heap_listp + (2 * WSIZE), PACK(DSIZE,1)); // 프롤로그의 푸터
+	PUT(heap_listp + (3 * WSIZE), PACK(0,1));		// 에필로그 헤더
+	heap_list += (2 * WSIZE);
+
+	if (extend_heap(CHUNKSIZE/WSIZE) == NULL) return -1;
+
     return 0;
 }
 
+static void *extend_heap(size_t words){
+	
+	char *bp;
+	size_t size;
+
+	// 
+	size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+	if ((long) (bp = mem_sbrk(size)) == -1) return NULL;
+
+	PUT(HDRP(bp), PACK(size, 0));
+	PUT(FTRP(bp), PACK(size, 0));
+	PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+
+	return coalesce(bp);
+}
 /* 
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
  */
+
+void *mm_malloc(size_t size)
+{
+	size_t asize; /* Adjusted block size */
+	size_t extendsize; /* Amount to extend heap if no fit */
+	char *bp;
+
+	/* Ignore spurious requests */
+	if (size == 0) return NULL;
+
+	/* Adjust block size to include overhead and alignment reqs. */
+	if (size <= DSIZE)
+		asize = 2*DSIZE;
+	else
+		asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);
+
+	/* Search the free list for a fit */
+	if ((bp = find_fit(asize)) != NULL) {
+		place(bp, asize);
+		return bp;
+	}
+
+	/* No fit found. Get more memory and place the block */
+	extendsize = MAX(asize,CHUNKSIZE);
+	if ((bp = extend_heap(extendsize/WSIZE)) == NULL)
+		return NULL;
+
+	place(bp, asize);
+	return bp;
+}
+
+static void *find_fit(size_t asize)
+{
+	/* First-fit search */
+	void *bp;
+
+	for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+		if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
+			return bp;
+		}
+	 }
+	return NULL; /* No fit */
+#endif
+}
+
+static void place(void *bp, size_t asize)
+{
+	size_t csize = GET_SIZE(HDRP(bp));
+
+	if ((csize - asize) >= (2*DSIZE)) {
+		PUT(HDRP(bp), PACK(asize, 1));
+		PUT(FTRP(bp), PACK(asize, 1));
+		bp = NEXT_BLKP(bp);
+		PUT(HDRP(bp), PACK(csize-asize, 0));
+		PUT(FTRP(bp), PACK(csize-asize, 0));
+	}
+	else {
+		PUT(HDRP(bp), PACK(csize, 1));
+		PUT(FTRP(bp), PACK(csize, 1));
+	}
+}
+
 void *mm_malloc(size_t size)
 {
     int newsize = ALIGN(size + SIZE_T_SIZE);
@@ -73,8 +202,51 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+	size_t size = GET_SIZE(HDRP(bp));
+
+	PUT (HDRP(bp), PACK(size, 0));
+	PUT (FTRP(bp), PACK(size, 0));
+
+	coalesce(bp);
 }
 
+static void *coalesce(void *bp) {
+
+	size_t preb
+
+}
+static void *coalesce(void *bp)
+{
+	size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+	size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+	size_t size = GET_SIZE(HDRP(bp));
+
+	if (prev_alloc && next_alloc) { /* Case 1 */
+	return bp;
+	}
+
+	else if (prev_alloc && !next_alloc) { /* Case 2 */
+	size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+	PUT(HDRP(bp), PACK(size, 0));
+	PUT(FTRP(bp), PACK(size,0));
+	}
+
+	else if (!prev_alloc && next_alloc) { /* Case 3 */
+	size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+	PUT(FTRP(bp), PACK(size, 0));
+	PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+	bp = PREV_BLKP(bp);
+	}
+
+	else { /* Case 4 */
+	size += GET_SIZE(HDRP(PREV_BLKP(bp))) +
+	GET_SIZE(FTRP(NEXT_BLKP(bp)));
+	PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+	PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+	bp = PREV_BLKP(bp);
+	}
+	return bp;
+}
 /*
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
  */
@@ -94,17 +266,5 @@ void *mm_realloc(void *ptr, size_t size)
     mm_free(oldptr);
     return newptr;
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
